da45127014de90408efc648721d90d76af75573b
Kavaeric <kavaeric@proton.me>
2025-07-13 20:43:26 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	basePath: "/airliner-chart",
 	output: "export",  // enables static exports for GitHub Pages
 	reactStrictMode: true,
 };
